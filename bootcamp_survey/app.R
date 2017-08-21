82ed7f43c2a8fedb355fc29b91aecdd0b9d472b2
d-bohn <dna5021@psu.edu>
2017-08-21 17:04:28 -0400

    added responses folder to bootcamp folder

--- bootcamp_survey/app.R
+++ bootcamp_survey/app.R
@@ -41,9 +41,9 @@ shinyApp(
     div(id = "form", # Display the questions
         textInput("title", labelMandatory("What is your title?"), "",
                   placeholder = "e.g., Grad, Faculty, etc."),
-        numericInput("age", label("What is your age?"),
+        numericInput("age", label = "What is your age?",
                      value = 0, min = 16, max = 100),
-        textInput("area", label("Program area"), "",
+        textInput("area", label = "Program area" , "",
                   placeholder = "e.g., Clinical, Developmental, etc."),
         checkboxGroupInput("days_attended", "Which days did you attend the workshop?",
                            c("Day 1 (Wednesday)" = "day1", "Day 2 (Thursday)" = "day2")),
